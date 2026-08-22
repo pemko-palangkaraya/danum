fceb5c6e6b6bda698f86cded6968ecc397afc90e
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-08-23 02:04:54 +0700

    Register tenant user middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureSuperAdmin;
+use App\Http\Middleware\EnsureTenantUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'superadmin' => EnsureSuperAdmin::class,
+            'tenant' => EnsureTenantUser::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
