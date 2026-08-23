cab68ce25681bec6693c58943e371dc8d4609858
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-08-23 17:33:39 +0700

    feat: expose outgoing letter withdrawal UI

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Livewire\Tenants\Index as TenantIndex;
 use App\Livewire\LetterTypes\Index as LetterTypeIndex;
 use App\Livewire\OutgoingLetters\Index as OutgoingLetterIndex;
 use App\Livewire\OutgoingLetters\Show as OutgoingLetterShow;
+use App\Livewire\OutgoingLetterWithdrawals\Index as OutgoingLetterWithdrawalIndex;
 use App\Livewire\Positions\Index as PositionIndex;
 use App\Http\Controllers\VerificationController;
 use App\Http\Controllers\OutgoingLetterController;
@@ -43,6 +44,7 @@ Route::middleware('auth')->group(function () {
     });
 
     Route::get('/outgoing-letters', OutgoingLetterIndex::class)->name('outgoing-letters.index');
+    Route::get('/outgoing-letter-withdrawals/{letter?}', OutgoingLetterWithdrawalIndex::class)->name('outgoing-letter-withdrawals.index');
     Route::get('/outgoing-letters/{id}/pdf', [OutgoingLetterController::class, 'downloadPdf'])->name('outgoing-letters.pdf');
     Route::get('/outgoing-letters/{id}', OutgoingLetterShow::class)->name('outgoing-letters.show');
 
