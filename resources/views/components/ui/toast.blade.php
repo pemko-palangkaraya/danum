eec76a60b849bfe434ca7d4a55fc1d69ef9fa320
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-08-29 16:03:43 +0700

    Fix toast close icon rendering

--- resources/views/components/ui/toast.blade.php
+++ resources/views/components/ui/toast.blade.php
@@ -131,14 +131,16 @@
             aria-label="Tutup notifikasi">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-7 w-7"
+                class="h-5 w-5"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
+                stroke-width="2.25"
                 stroke-linecap="round"
+                stroke-linejoin="round"
                 aria-hidden="true">
-                <path d="M6 6l12 12M18 6L6 18" />
+                <path d="M6 6l12 12" />
+                <path d="M18 6L6 18" />
             </svg>
         </button>
 
