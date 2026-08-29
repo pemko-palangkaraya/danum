1f028364013647e36999b3df14e37a8489c17fef
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-08-29 16:49:35 +0700

    Fix TenantCategory model namespaces

--- app/Models/TenantCategory.php
+++ app/Models/TenantCategory.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace AppModels;
+namespace App\Models;
 
-use IlluminateDatabaseEloquentFactoriesHasFactory;
-use IlluminateDatabaseEloquentModel;
-use IlluminateDatabaseEloquentRelationsHasMany;
+use Illuminate\Database\Eloquent\Concerns\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TenantCategory extends Model
 {
