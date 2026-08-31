548b50e37b3ab512c1fb96401b37b16f5945a846
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-08-31 07:04:08 +0700

    Split landing page footer into partial

---
+++ resources/views/landing/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="footer-inner">
+        <span>© {{ date('Y') }} DANUM</span>
+        <span>Platform administrasi persuratan digital</span>
+    </div>
+</footer>
