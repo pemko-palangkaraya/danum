4307bacacd0317abfb07d0aa9acab8292404f62a
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-08-31 19:30:13 +0700

    fix: seed tenants across Indonesian administrative levels

--- database/seeders/TenantReferenceSeeder.php
+++ database/seeders/TenantReferenceSeeder.php
@@ -14,23 +14,26 @@ class TenantReferenceSeeder extends Seeder
     public function run(): void
     {
         $categories = [
+            ['pemerintah-pusat', 'Pemerintah Pusat'],
+            ['pemerintah-provinsi', 'Pemerintah Provinsi'],
+            ['pemerintah-kabupaten', 'Pemerintah Kabupaten'],
+            ['pemerintah-kota', 'Pemerintah Kota'],
             ['sekretariat-daerah', 'Sekretariat Daerah'],
             ['sekretariat-dprd', 'Sekretariat DPRD'],
             ['inspektorat', 'Inspektorat'],
             ['dinas', 'Dinas'],
             ['badan', 'Badan'],
-            ['satuan-polisi-pamong-praja', 'Satuan Polisi Pamong Praja'],
+            ['satpol-pp', 'Satuan Polisi Pamong Praja'],
             ['kecamatan', 'Kecamatan'],
             ['kelurahan', 'Kelurahan'],
-            ['uptd', 'UPTD'],
-            ['unit-pelaksana-teknis', 'Unit Pelaksana Teknis'],
-            ['rumah-sakit-daerah', 'Rumah Sakit Daerah'],
-            ['puskesmas', 'Puskesmas'],
-            ['blud', 'Badan Layanan Umum Daerah (BLUD)'],
             ['desa', 'Pemerintahan Desa'],
+            ['uptd', 'UPTD'],
+            ['puskesmas', 'Puskesmas'],
+            ['rumah-sakit-daerah', 'Rumah Sakit Daerah'],
+            ['blud', 'Badan Layanan Umum Daerah (BLUD)'],
+            ['satuan-pendidikan', 'Satuan Pendidikan'],
             ['bumn', 'Badan Usaha Milik Negara (BUMN)'],
             ['bumd', 'Badan Usaha Milik Daerah (BUMD)'],
-            ['pendidikan', 'Satuan Pendidikan'],
             ['lainnya', 'Lainnya'],
         ];
 
@@ -44,26 +47,48 @@ class TenantReferenceSeeder extends Seeder
         $category = fn (string $code) => TenantCategory::where('code', $code)->value('id');
 
         $tenants = [
-            ['setda-palangka-raya', 'Pemerintah Kota Palangka Raya - Sekretariat Daerah', 'sekretariat-daerah', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Menteng', 'Wali Kota Palangka Raya', 'Sekretaris Daerah'],
-            ['dprd-palangka-raya', 'DPRD Kota Palangka Raya - Sekretariat DPRD', 'sekretariat-dprd', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Ketua DPRD Kota Palangka Raya', 'Sekretaris DPRD'],
+            // Pemerintah pusat
+            ['pemerintah-indonesia', 'Pemerintah Republik Indonesia', 'pemerintah-pusat', 'DKI Jakarta', 'Jakarta Pusat', 'Gambir', 'Gambir', 'Presiden Republik Indonesia', 'Presiden'],
+
+            // Pemerintah provinsi
+            ['pemprov-kalimantan-tengah', 'Pemerintah Provinsi Kalimantan Tengah', 'pemerintah-provinsi', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Gubernur Kalimantan Tengah', 'Gubernur'],
+            ['pemprov-dki-jakarta', 'Pemerintah Provinsi DKI Jakarta', 'pemerintah-provinsi', 'DKI Jakarta', 'Jakarta Pusat', 'Gambir', 'Gambir', 'Gubernur DKI Jakarta', 'Gubernur'],
+
+            // Kabupaten
+            ['pemkab-kapuas', 'Pemerintah Kabupaten Kapuas', 'pemerintah-kabupaten', 'Kalimantan Tengah', 'Kuala Kapuas', 'Selat', 'Selat Hilir', 'Bupati Kapuas', 'Bupati'],
+            ['pemkab-kotawaringin-timur', 'Pemerintah Kabupaten Kotawaringin Timur', 'pemerintah-kabupaten', 'Kalimantan Tengah', 'Sampit', 'Mentawa Baru Ketapang', 'Mentawa Baru Hulu', 'Bupati Kotawaringin Timur', 'Bupati'],
+            ['pemkab-banjar', 'Pemerintah Kabupaten Banjar', 'pemerintah-kabupaten', 'Kalimantan Selatan', 'Martapura', 'Martapura', 'Jawa', 'Bupati Banjar', 'Bupati'],
+
+            // Kota
+            ['pemkot-palangka-raya', 'Pemerintah Kota Palangka Raya', 'pemerintah-kota', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Langkai', 'Wali Kota Palangka Raya', 'Wali Kota'],
+            ['pemkot-banjarmasin', 'Pemerintah Kota Banjarmasin', 'pemerintah-kota', 'Kalimantan Selatan', 'Banjarmasin', 'Banjarmasin Tengah', 'Kertak Baru Ulu', 'Wali Kota Banjarmasin', 'Wali Kota'],
+            ['pemkot-surakarta', 'Pemerintah Kota Surakarta', 'pemerintah-kota', 'Jawa Tengah', 'Surakarta', 'Banjarsari', 'Manahan', 'Wali Kota Surakarta', 'Wali Kota'],
+
+            // Perangkat daerah
+            ['setda-palangka-raya', 'Sekretariat Daerah Kota Palangka Raya', 'sekretariat-daerah', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Sekretaris Daerah Kota Palangka Raya', 'Sekretaris Daerah'],
+            ['dprd-palangka-raya', 'Sekretariat DPRD Kota Palangka Raya', 'sekretariat-dprd', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Sekretaris DPRD Kota Palangka Raya', 'Sekretaris DPRD'],
             ['inspektorat-palangka-raya', 'Inspektorat Kota Palangka Raya', 'inspektorat', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Inspektur Kota Palangka Raya', 'Inspektur'],
             ['dinkes-palangka-raya', 'Dinas Kesehatan Kota Palangka Raya', 'dinas', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala Dinas Kesehatan', 'Kepala Dinas'],
-            ['disdik-palangka-raya', 'Dinas Pendidikan Kota Palangka Raya', 'dinas', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala Dinas Pendidikan', 'Kepala Dinas'],
             ['disdukcapil-palangka-raya', 'Dinas Kependudukan dan Pencatatan Sipil Kota Palangka Raya', 'dinas', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala Dinas Dukcapil', 'Kepala Dinas'],
-            ['dpmptsp-palangka-raya', 'DPMPTSP Kota Palangka Raya', 'dinas', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala DPMPTSP', 'Kepala Dinas'],
-            ['dinsos-palangka-raya', 'Dinas Sosial Kota Palangka Raya', 'dinas', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala Dinas Sosial', 'Kepala Dinas'],
-            ['bappeda-palangka-raya', 'Bappeda Kota Palangka Raya', 'badan', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala Bappeda', 'Kepala Badan'],
-            ['bpkad-palangka-raya', 'BPKAD Kota Palangka Raya', 'badan', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala BPKAD', 'Kepala Badan'],
-            ['satpol-pp-palangka-raya', 'Satuan Polisi Pamong Praja Kota Palangka Raya', 'satuan-polisi-pamong-praja', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Langkai', 'Kepala Satpol PP', 'Kepala Satuan'],
+            ['bappeda-palangka-raya', 'Badan Perencanaan Pembangunan Daerah Kota Palangka Raya', 'badan', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Kepala Bappeda', 'Kepala Badan'],
+            ['satpol-pp-palangka-raya', 'Satuan Polisi Pamong Praja Kota Palangka Raya', 'satpol-pp', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Langkai', 'Kepala Satpol PP', 'Kepala Satuan'],
+
+            // Kecamatan, kelurahan, desa
             ['kecamatan-pahandut', 'Kecamatan Pahandut', 'kecamatan', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Langkai', 'Camat Pahandut', 'Camat'],
             ['kecamatan-jekan-raya', 'Kecamatan Jekan Raya', 'kecamatan', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Camat Jekan Raya', 'Camat'],
-            ['kelurahan-langakai', 'Kelurahan Langkai', 'kelurahan', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Langkai', 'Lurah Langkai', 'Lurah'],
+            ['kelurahan-langkai', 'Kelurahan Langkai', 'kelurahan', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Langkai', 'Lurah Langkai', 'Lurah'],
+            ['kelurahan-menteng', 'Kelurahan Menteng', 'kelurahan', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Lurah Menteng', 'Lurah'],
+            ['desa-tumbang-talang', 'Pemerintah Desa Tumbang Talang', 'desa', 'Kalimantan Tengah', 'Gunung Mas', 'Kurun', 'Tumbang Talang', 'Kepala Desa Tumbang Talang', 'Kepala Desa'],
+            ['desa-bukit-rawi', 'Pemerintah Desa Bukit Rawi', 'desa', 'Kalimantan Tengah', 'Pulang Pisau', 'Kahayan Tengah', 'Bukit Rawi', 'Kepala Desa Bukit Rawi', 'Kepala Desa'],
+
+            // Unit layanan
             ['rsud-doris-sylvanus', 'RSUD dr. Doris Sylvanus', 'rumah-sakit-daerah', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Palangka', 'Direktur RSUD dr. Doris Sylvanus', 'Direktur'],
-            ['puskesmas-pahandut', 'UPT Puskesmas Pahandut', 'puskesmas', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Pahandut', 'Kepala UPT Puskesmas Pahandut', 'Kepala Puskesmas'],
-            ['smkn-1-palangka-raya', 'SMK Negeri 1 Palangka Raya', 'pendidikan', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Bukit Tunggal', 'Kepala SMK Negeri 1 Palangka Raya', 'Kepala Sekolah'],
-            ['desa-contoh', 'Pemerintah Desa Contoh', 'desa', 'Kalimantan Tengah', 'Kotawaringin Timur', 'Baamang', 'Desa Contoh', 'Kepala Desa Contoh', 'Kepala Desa'],
+            ['puskesmas-pahandut', 'UPT Puskesmas Pahandut', 'puskesmas', 'Kalimantan Tengah', 'Palangka Raya', 'Pahandut', 'Pahandut', 'Kepala Puskesmas Pahandut', 'Kepala Puskesmas'],
+            ['smkn-1-palangka-raya', 'SMK Negeri 1 Palangka Raya', 'satuan-pendidikan', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Bukit Tunggal', 'Kepala SMK Negeri 1 Palangka Raya', 'Kepala Sekolah'],
+
+            // BUMN/BUMD sebagai contoh organisasi non-pemerintahan daerah
             ['bumn-contoh', 'BUMN Contoh Indonesia', 'bumn', 'DKI Jakarta', 'Jakarta Pusat', 'Gambir', 'Gambir', 'Direktur Utama', 'Direktur Utama'],
-            ['bumd-contoh', 'BUMD Contoh Indonesia', 'bumd', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Direktur Utama', 'Direktur Utama'],
+            ['bumd-palangka-raya', 'BUMD Kota Palangka Raya', 'bumd', 'Kalimantan Tengah', 'Palangka Raya', 'Jekan Raya', 'Menteng', 'Direktur Utama', 'Direktur Utama'],
         ];
 
         foreach ($tenants as [$code, $name, $categoryCode, $province, $city, $district, $village, $headName, $headTitle]) {
