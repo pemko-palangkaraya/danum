fe5c19c8f575951f0ee29ee5e320ba58b6b60fdc
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-04 19:57:48 +0700

    Fix statistics header partial data binding

--- resources/views/livewire/population/statistics.blade.php
+++ resources/views/livewire/population/statistics.blade.php
@@ -1,5 +1,8 @@
 <div class="space-y-6">
-    @include('livewire.population.partials.statistics-header')
+    @include('livewire.population.partials.statistics-header', [
+        'isSuperAdmin' => $isSuperAdmin,
+        'tenants' => $tenants,
+    ])
     @include('livewire.population.partials.statistics-summary')
     @include('livewire.population.partials.statistics-age-pyramid')
     @include('livewire.population.partials.statistics-age-summary')
