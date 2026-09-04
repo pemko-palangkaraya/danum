e29e6d1361da90e0ed6b74d0b30983ba3b5626d8
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-04 20:19:58 +0700

    refactor(population): compose citizens page without auth logic

--- resources/views/livewire/pages/population/citizens.blade.php
+++ resources/views/livewire/pages/population/citizens.blade.php
@@ -1,12 +1,24 @@
 <div class="space-y-6">
-    @include('livewire.pages.population.partials.citizens-header')
-    @include('livewire.pages.population.partials.citizens-filters')
+    @include('livewire.pages.population.partials.citizens-header', [
+        'isSuperAdmin' => $isSuperAdmin,
+        'canView' => true,
+        'canManage' => $canManage,
+        'tenantSelected' => $tenantSelected,
+    ])
 
-    @if($selectedTenantId || auth()->user()->tenant_id)
+    @include('livewire.pages.population.partials.citizens-filters', [
+        'isSuperAdmin' => $isSuperAdmin,
+        'tenants' => $tenants,
+    ])
+
+    @if($tenantSelected)
         @if($showForm)
             @include('livewire.pages.population.partials.citizens-form')
         @endif
 
-        @include('livewire.pages.population.partials.citizens-table')
+        @include('livewire.pages.population.partials.citizens-table', [
+            'canManage' => $canManage,
+            'detailRoute' => $detailRoute,
+        ])
     @endif
 </div>
