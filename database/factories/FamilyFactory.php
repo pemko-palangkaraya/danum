86c841b4e404dceec5c1b5fe748462904e2c2371
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-05 07:39:55 +0700

    refactor family factory for realistic Indonesian addresses

--- database/factories/FamilyFactory.php
+++ database/factories/FamilyFactory.php
@@ -15,18 +15,35 @@ class FamilyFactory extends Factory
 
     public function definition(): array
     {
+        $locations = [
+            ['kelurahan' => 'Langkai', 'kecamatan' => 'Pahandut', 'kode_pos' => '73111'],
+            ['kelurahan' => 'Pahandut', 'kecamatan' => 'Pahandut', 'kode_pos' => '73112'],
+            ['kelurahan' => 'Menteng', 'kecamatan' => 'Jekan Raya', 'kode_pos' => '73111'],
+            ['kelurahan' => 'Palangka', 'kecamatan' => 'Jekan Raya', 'kode_pos' => '73112'],
+            ['kelurahan' => 'Panarung', 'kecamatan' => 'Pahandut', 'kode_pos' => '73121'],
+            ['kelurahan' => 'Bukit Tunggal', 'kecamatan' => 'Jekan Raya', 'kode_pos' => '73113'],
+            ['kelurahan' => 'Kereng Bangkirai', 'kecamatan' => 'Sabangau', 'kode_pos' => '73126'],
+            ['kelurahan' => 'Sabaru', 'kecamatan' => 'Sabangau', 'kode_pos' => '73123'],
+            ['kelurahan' => 'Tangkiling', 'kecamatan' => 'Bukit Batu', 'kode_pos' => '73127'],
+            ['kelurahan' => 'Banturung', 'kecamatan' => 'Bukit Batu', 'kode_pos' => '73113'],
+        ];
+        $location = $this->faker->randomElement($locations);
+
         return [
             'tenant_id' => Tenant::factory(),
-            'no_kk' => $this->faker->unique()->numerify('62##############'),
+            'no_kk' => $this->faker->unique()->numerify('6271############'),
             'head_citizen_id' => null,
-            'alamat' => $this->faker->streetAddress(),
-            'rt' => $this->faker->numerify('###'),
-            'rw' => $this->faker->numerify('###'),
-            'kelurahan' => $this->faker->citySuffix(),
-            'kecamatan' => $this->faker->citySuffix(),
+            'alamat' => 'Jl. '.$this->faker->randomElement([
+                'Antang Kalang', 'Cilik Riwut', 'Rajawali', 'G. Obos', 'Yos Sudarso',
+                'Diponegoro', 'RTA Milono', 'Adonis Samad', 'Tjilik Riwut',
+            ]).' No. '.$this->faker->numberBetween(1, 180),
+            'rt' => str_pad((string) $this->faker->numberBetween(1, 30), 3, '0', STR_PAD_LEFT),
+            'rw' => str_pad((string) $this->faker->numberBetween(1, 15), 3, '0', STR_PAD_LEFT),
+            'kelurahan' => $location['kelurahan'],
+            'kecamatan' => $location['kecamatan'],
             'kabupaten_kota' => 'Palangka Raya',
             'provinsi' => 'Kalimantan Tengah',
-            'kode_pos' => $this->faker->numerify('73###'),
+            'kode_pos' => $location['kode_pos'],
             'status' => 'active',
         ];
     }
