8cf002a1cc89390b54fcc350650c8876a85acdec
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-05 19:27:45 +0700

    feat: add DANUM CA initialization command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Services\CertificateAuthorityService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('danum:ca-init', function (CertificateAuthorityService $service): void {
+    [$root, $issuing] = $service->ensureHierarchy();
+
+    $this->info('DANUM CA hierarchy siap.');
+    $this->line('Root CA:    '.$root->name.' | '.$root->fingerprint_sha256);
+    $this->line('Issuing CA: '.$issuing->name.' | '.$issuing->fingerprint_sha256);
+})->purpose('Initialize the DANUM Root CA and Issuing CA hierarchy');
