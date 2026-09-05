5e2fae1371bd7866579c84025c91b713e2068e69
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-05 08:28:09 +0700

    update population demo test for Mungku Baru

--- tests/Feature/Population/PopulationDemoSeederTest.php
+++ tests/Feature/Population/PopulationDemoSeederTest.php
@@ -22,8 +22,8 @@ class PopulationDemoSeederTest extends TestCase
             'code' => 'DEMO001',
             'province' => 'Kalimantan Tengah',
             'city' => 'Palangka Raya',
-            'district' => 'Pahandut',
-            'village' => 'Langkai',
+            'district' => 'Rakumpit',
+            'village' => 'Mungku Baru',
             'status' => TenantStatus::ACTIVE,
         ]);
 
@@ -32,10 +32,11 @@ class PopulationDemoSeederTest extends TestCase
         $this->assertDatabaseCount('families', 20);
         $this->assertDatabaseHas('families', [
             'tenant_id' => $tenant->id,
-            'kelurahan' => 'Langkai',
-            'kecamatan' => 'Pahandut',
+            'kelurahan' => 'Mungku Baru',
+            'kecamatan' => 'Rakumpit',
             'kabupaten_kota' => 'Palangka Raya',
             'provinsi' => 'Kalimantan Tengah',
+            'kode_pos' => '73229',
         ]);
 
         $this->assertSame(1, DB::table('families')->distinct()->count('kelurahan'));
