9c8901d7dc1e4ab5e9d9fda3bbb62f1e8d51497a
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-07 07:44:44 +0700

    feat: include roman month in letter numbering

--- database/seeders/LetterClassificationSeeder.php
+++ database/seeders/LetterClassificationSeeder.php
@@ -18,7 +18,7 @@ class LetterClassificationSeeder extends Seeder
             ['300', 'Keamanan dan Ketertiban', 'Klasifikasi urusan keamanan dan ketertiban.', 4],
             ['400', 'Kesejahteraan Rakyat', 'Klasifikasi urusan kesejahteraan rakyat.', 5],
             ['500', 'Perekonomian', 'Klasifikasi urusan perekonomian.', 6],
-            ['600', 'Pekerjaan Umum', 'Klasifikasi urusan pekerjaan umum dan pembangunan.', 7],
+            ['600', 'Pekerjaan Umum dan Ketenagaan', 'Klasifikasi urusan pekerjaan umum dan ketenagaan.', 7],
             ['700', 'Pengawasan', 'Klasifikasi urusan pengawasan.', 8],
             ['800', 'Kepegawaian', 'Klasifikasi urusan kepegawaian.', 9],
             ['900', 'Keuangan', 'Klasifikasi urusan keuangan.', 10],
@@ -30,7 +30,7 @@ class LetterClassificationSeeder extends Seeder
                 [
                     'name' => $name,
                     'description' => $description,
-                    'number_format' => '{{number}}/{{classification_code}}/{{tenant_code}}/{{year}}',
+                    'number_format' => '{{number}}/{{classification_code}}/{{tenant_code}}/{{month_roman}}/{{year}}',
                     'number_padding' => 3,
                     'sort_order' => $sortOrder,
                     'is_active' => true,
