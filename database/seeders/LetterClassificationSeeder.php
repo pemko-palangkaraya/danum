46e754fc5a9e3f2a48edff08c8f6bdccd9a78bb6
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-07 08:30:02 +0700

    Seed common Permendagri 83 classifications for Assistant

--- database/seeders/LetterClassificationSeeder.php
+++ database/seeders/LetterClassificationSeeder.php
@@ -12,6 +12,7 @@ class LetterClassificationSeeder extends Seeder
     public function run(): void
     {
         $source = 'Permendagri Nomor 83 Tahun 2022';
+
         $classifications = [
             ['000', 'Umum', 'Klasifikasi umum dan administrasi umum.', 1],
             ['100', 'Pemerintahan', 'Klasifikasi urusan pemerintahan.', 2],
@@ -23,16 +24,26 @@ class LetterClassificationSeeder extends Seeder
             ['700', 'Pengawasan', 'Klasifikasi urusan pengawasan.', 8],
             ['800', 'Kepegawaian', 'Klasifikasi urusan kepegawaian.', 9],
             ['900', 'Keuangan', 'Klasifikasi urusan keuangan.', 10],
+
+            // Klasifikasi yang paling sering menjadi tujuan surat layanan masyarakat.
+            ['400.7.22', 'Surat Keterangan, Sertifikasi dan Perijinan', 'Surat keterangan, sertifikasi dan perijinan.', 400722],
+            ['400.7.22.1', 'Surat keterangan', 'Surat keterangan yang diterbitkan pemerintah daerah.', 4007221],
+            ['400.12', 'Kependudukan dan Catatan Sipil', 'Urusan kependudukan dan catatan sipil.', 4001200],
+            ['400.12.4', 'Pengelolaan Informasi Administrasi Kependudukan', 'Pengelolaan informasi administrasi kependudukan.', 4001240],
+            ['400.12.4.4', 'Penyajian dan Layanan Informasi Administrasi Kependudukan', 'Penyajian dan layanan informasi administrasi kependudukan.', 4001244],
+            ['500.5.7.15', 'Rekomendasi', 'Klasifikasi untuk dokumen rekomendasi.', 5005715],
+            ['800.1.11.1', 'Surat Perintah Dinas/Surat Tugas', 'Surat perintah dinas dan surat tugas.', 8001111],
+            ['000.1.5', 'Rapat pimpinan antara lain: Notula/Risalah Rapat', 'Administrasi rapat pimpinan dan notula/risalah rapat.', 105],
         ];
 
         foreach ($classifications as [$code, $name, $description, $sourceOrder]) {
             LetterClassification::query()->updateOrCreate(
-                ['code' => $code],
+                ['source_order' => $sourceOrder],
                 [
+                    'code' => $code,
                     'name' => $name,
                     'description' => $description,
                     'source' => $source,
-                    'source_order' => $sourceOrder,
                     'number_format' => '{{number}}/{{classification_code}}/{{tenant_code}}/{{month_roman}}/{{year}}',
                     'number_padding' => 3,
                     'sort_order' => $sourceOrder,
