85860285674e20324671e608357ef4f7629c30fa
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-07 11:08:04 +0700

    show only category letter type permissions

--- resources/views/livewire/pages/letter-types/permissions.blade.php
+++ resources/views/livewire/pages/letter-types/permissions.blade.php
@@ -1,17 +1,6 @@
 <div class="space-y-6">
     @include('livewire.pages.letter-types.partials.permissions-header')
-    @include('livewire.pages.letter-types.partials.permissions-tenants')
     @include('livewire.pages.letter-types.partials.permissions-categories')
-
-    <x-ui.confirmation-modal
-        modal-id="letter-type-permission-revoke"
-        title="Cabut Akses OPD"
-        :message="'Apakah Anda yakin ingin mencabut akses '.$selectedTenantName.' untuk jenis surat ini?'"
-        confirm-text="Cabut Akses"
-        cancel-text="Batal"
-        confirm-action="revoke"
-        cancel-action="cancelRevoke"
-        variant="danger" />
 
     <x-ui.confirmation-modal
         modal-id="letter-type-category-permission-revoke"
