e655ef765add35b03b4cbf4f18062dfee85dc058
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-09 00:13:06 +0700

    Add required and invalid semantics to select component

--- resources/views/components/ui/select.blade.php
+++ resources/views/components/ui/select.blade.php
@@ -9,6 +9,8 @@
 <x-ui.field :label="$label" :error="$error" :hint="$hint" :required="$required" :for="$id">
     <select
         @if($id) id="{{ $id }}" @endif
+        @if($required) required @endif
+        @if($error) aria-invalid="true" @endif
         {{ $attributes->merge(['class' => 'form-select w-full']) }}>
         {{ $slot }}
     </select>
