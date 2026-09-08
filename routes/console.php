adca9bcfdb59977b8e260dad4cfb7a50620e2a45
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-08 11:16:51 +0700

    feat schedule letter type deletion processor

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Services\CertificateAuthorityService;
+use App\Services\LetterTypeService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
@@ -15,3 +17,7 @@ Artisan::command('danum:ca-init', function (CertificateAuthorityService $service
     $this->line('Root CA:    '.$root->name.' | '.$root->fingerprint_sha256);
     $this->line('Issuing CA: '.$issuing->name.' | '.$issuing->fingerprint_sha256);
 })->purpose('Initialize the DANUM Root CA and Issuing CA hierarchy');
+
+Schedule::call(function (LetterTypeService $service): void {
+    $service->processScheduledDeletions();
+})->everyMinute()->withoutOverlapping();
