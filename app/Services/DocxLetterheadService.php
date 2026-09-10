e3f207346997d3c87de2df867a1df049e664a32f
pemko-palangkaraya <pemko.palangkaraya91@gmail.com>
2026-09-10 23:08:23 +0700

    fix: preserve letterhead logo aspect ratio

--- app/Services/DocxLetterheadService.php
+++ app/Services/DocxLetterheadService.php
@@ -49,7 +49,7 @@ class DocxLetterheadService
             $relsDom->documentElement?->appendChild($rel);
         }
 
-        $tableXml = $this->buildTableXml($tenant, $mediaName, $logoPath !== null);
+        $tableXml = $this->buildTableXml($tenant, $mediaName, $logoPath !== null, $logoPath);
         foreach ($targets as $paragraph) {
             $parent = $paragraph->parentNode;
             if (! $parent) continue;
@@ -115,7 +115,7 @@ class DocxLetterheadService
         return ['xml' => $dom->saveXML() ?: '', 'rels' => $relsDom->saveXML() ?: $rels, 'contentTypes' => $contentTypes, 'mediaName' => $mediaName, 'mediaPath' => $imagePath];
     }
 
-    private function buildTableXml(Tenant $tenant, string $mediaName, bool $hasLogo): string
+    private function buildTableXml(Tenant $tenant, string $mediaName, bool $hasLogo, ?string $logoPath = null): string
     {
         $rows = [];
         foreach ([[$tenant->letterhead_line1, (int) ($tenant->letterhead_line1_size ?? 15)], [$tenant->letterhead_line2, (int) ($tenant->letterhead_line2_size ?? 13)], [$tenant->letterhead_line3, (int) ($tenant->letterhead_line3_size ?? 11)]] as [$value, $size]) {
@@ -131,9 +131,31 @@ class DocxLetterheadService
         $grid = $hasLogo ? '<w:gridCol w:w="1800"/><w:gridCol w:w="7200"/>' : '<w:gridCol w:w="9000"/>';
         $textCell = '<w:tc><w:tcPr><w:tcW w:w="' . $textWidth . '" w:type="dxa"/><w:vAlign w:val="center"/><w:tcMar><w:left w:w="120" w:type="dxa"/><w:right w:w="120" w:type="dxa"/></w:tcMar></w:tcPr>' . implode('', $rows) . '</w:tc>';
         $logoCell = '';
-        if ($hasLogo) $logoCell = '<w:tc><w:tcPr><w:tcW w:w="1800" w:type="dxa"/><w:vAlign w:val="center"/><w:tcMar><w:left w:w="60" w:type="dxa"/><w:right w:w="120" w:type="dxa"/></w:tcMar></w:tcPr><w:p><w:pPr><w:jc w:val="center"/><w:spacing w:before="0" w:after="0"/></w:pPr><w:r><w:drawing><wp:inline xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture" xmlns:r="' . self::OFFICE_REL_NS . '" distT="0" distB="0" distL="0" distR="0"><wp:extent cx="1100000" cy="1100000"/><wp:docPr id="9002" name="DANUM Letterhead Logo"/><a:graphic><a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture"><pic:pic><pic:nvPicPr><pic:cNvPr id="0" name="' . htmlspecialchars($mediaName, ENT_XML1 | ENT_QUOTES, 'UTF-8') . '"/><pic:cNvPicPr/></pic:nvPicPr><pic:blipFill><a:blip r:embed="rIdDanumLetterheadLogo"/><a:stretch><a:fillRect/></a:stretch></pic:blipFill><pic:spPr><a:xfrm><a:off x="0" y="0"/><a:ext cx="1100000" cy="1100000"/></a:xfrm><a:prstGeom prst="rect"><a:avLst/></a:prstGeom></pic:spPr></pic:pic></a:graphicData></a:graphic></wp:inline></w:drawing></w:r></w:p></w:tc>';
+        if ($hasLogo) {
+            $logoExtent = $this->calculateLogoExtent($logoPath);
+            $cx = $logoExtent['cx'];
+            $cy = $logoExtent['cy'];
+            $logoCell = '<w:tc><w:tcPr><w:tcW w:w="1800" w:type="dxa"/><w:vAlign w:val="center"/><w:tcMar><w:left w:w="60" w:type="dxa"/><w:right w:w="120" w:type="dxa"/></w:tcMar></w:tcPr><w:p><w:pPr><w:jc w:val="center"/><w:spacing w:before="0" w:after="0"/></w:pPr><w:r><w:drawing><wp:inline xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture" xmlns:r="' . self::OFFICE_REL_NS . '" distT="0" distB="0" distL="0" distR="0"><wp:extent cx="' . $cx . '" cy="' . $cy . '"/><wp:docPr id="9002" name="DANUM Letterhead Logo"/><a:graphic><a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture"><pic:pic><pic:nvPicPr><pic:cNvPr id="0" name="' . htmlspecialchars($mediaName, ENT_XML1 | ENT_QUOTES, 'UTF-8') . '"/><pic:cNvPicPr/></pic:nvPicPr><pic:blipFill><a:blip r:embed="rIdDanumLetterheadLogo"/><a:stretch><a:fillRect/></a:stretch></pic:blipFill><pic:spPr><a:xfrm><a:off x="0" y="0"/><a:ext cx="' . $cx . '" cy="' . $cy . '"/></a:xfrm><a:prstGeom prst="rect"><a:avLst/></a:prstGeom></pic:spPr></pic:pic></a:graphicData></a:graphic></wp:inline></w:drawing></w:r></w:p></w:tc>';
+        }
 
         return '<w:tbl xmlns:w="' . DocxRendererService::WORD_NS . '"><w:tblPr><w:tblW w:w="9000" w:type="dxa"/><w:tblLayout w:type="fixed"/><w:jc w:val="left"/><w:tblCellMar><w:top w:w="0" w:type="dxa"/><w:left w:w="0" w:type="dxa"/><w:bottom w:w="0" w:type="dxa"/><w:right w:w="0" w:type="dxa"/></w:tblCellMar><w:tblBorders><w:top w:val="nil"/><w:left w:val="nil"/><w:bottom w:val="single" w:sz="16" w:space="1"/><w:right w:val="nil"/><w:insideH w:val="nil"/><w:insideV w:val="nil"/></w:tblBorders></w:tblPr><w:tblGrid>' . $grid . '</w:tblGrid><w:tr>' . $logoCell . $textCell . '</w:tr></w:tbl>';
+    }
+
+    /** @return array{cx:int,cy:int} */
+    private function calculateLogoExtent(?string $logoPath): array
+    {
+        $maxCx = 1400000;
+        $maxCy = 1400000;
+        $size = $logoPath !== null ? @getimagesize($logoPath) : false;
+        $width = (int) ($size[0] ?? 1);
+        $height = (int) ($size[1] ?? 1);
+        if ($width <= 0 || $height <= 0) return ['cx' => $maxCx, 'cy' => $maxCy];
+
+        $scale = min($maxCx / $width, $maxCy / $height);
+        return [
+            'cx' => max(1, (int) round($width * $scale)),
+            'cy' => max(1, (int) round($height * $scale)),
+        ];
     }
 
     private function textParagraph(string $text, int $points, bool $bold): string
